30353f0c94ee76e929a79e097a9180e56cb0e42e
Jitendra <jitendra@Jitendra.local>
2019-11-09 16:15:43 +0530

    addded cors

--- app.js
+++ app.js
@@ -28,11 +28,16 @@ app.use('/createlead',createPLRecRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
