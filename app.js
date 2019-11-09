c8c89fcca5d38ff98c3427e4e19ab6b8f56d9905
Jitendra <jitendra@Jitendra.local>
2019-11-09 19:49:57 +0530

    added cors

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ var createPLRecRouter = require('./routes/createPLead');
 
 var app = express();
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next(createError(404));
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -26,12 +33,7 @@ app.use('/', indexRouter);
 //app.use('/verify',credsRouter);
 app.use('/createlead',createPLRecRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
