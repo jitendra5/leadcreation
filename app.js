3563cbebce9b4ef9e17950cd6dee3b6e92b09b25
Jitendra Josyula <jitendra.jjr@gmail.com>
2020-03-31 21:04:53 +0530

    updated app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 //var credsRouter = require('./routes/verifysfdc');
 var createPLRecRouter = require('./routes/createPLead');
+var phoneCountryCodes = require('./routes/phoneCountryCodes');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 //app.use('/verify',credsRouter);
 app.use('/createlead',createPLRecRouter);
+app.use('/getphone',phoneCountryCodes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
