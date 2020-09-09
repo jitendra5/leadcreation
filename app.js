416dec512be11a84b1a5499e6b1ed27593837a61
Jitendra Josyula <jitendra.jjr@gmail.com>
2020-09-09 16:54:18 +0530

    uncomented vl lead in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 //var credsRouter = require('./routes/verifysfdc');
-//var createPLRecRouter = require('./routes/createPLead');
+var createPLRecRouter = require('./routes/createPLead');
 var createLRecRouter = require('./routes/createLead');
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 //app.use('/verify',credsRouter);
-//app.use('/createlead',createPLRecRouter);
+app.use('/createlead',createPLRecRouter);
 app.use('/createbdlead',createLRecRouter);
 
 // catch 404 and forward to error handler
